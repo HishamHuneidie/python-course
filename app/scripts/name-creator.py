b03b660bffefbdb426578d827cfbd10e91aad711
Hisham <hhuneidie@gmail.com>
2026-04-18 13:48:38 +0200

    feat: Create a business-name-generator

---
+++ app/scripts/name-creator.py
@@ -0,0 +1,6 @@
+city = input('Cual es tu ciudad soñada? ')
+color = input('Cual es tu color favorito? ')
+
+print('Felicidades!!! Aquí hay algunas sugerencias para tu negocio.')
+print(' - ' + city + ' ' + color)
+print(' - ' + color + ' ' + city)
