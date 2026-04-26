1ee35faf12dbfbb9ed0b53d1046b736f3ae1483f
Hisham <hhuneidie@gmail.com>
2026-04-26 17:34:32 +0200

    feat: Math operations

---
+++ app/scripts/02-commission-calculator.py
@@ -0,0 +1,34 @@
+name = input('Cual es tu nombre? ')
+sales = float(input('Cuanto has vendido este mes? '))
+
+commissions = round(sales * .13, 2)
+
+print(f"Bien {name}, tus comisiones son: {commissions}")
+
+# Prácticas del segundo módulo
+
+# x = 4
+# y = 10
+# a = 2
+# b = 3
+# c = 5
+#
+# print("Mis números son {} y {}".format(x,y))
+# print(f"Mis números son {x} y {y}")
+#
+# print()
+# print("Operaciones matemáticas:")
+# print()
+#
+# print(f"{a} más {b} igual a = {a + b}")
+# print(f"{a} menos {b} igual a = {a - b}")
+# print(f"{a} por {b} igual a = {a * b}")
+# print(f"{a} entre {b} igual a = {a / b}")
+#
+# print(f"{a} dividido al piso por {b} igual a = {a // b}")
+# print(f"{a} módulo {b} igual a = {a % b}")
+#
+# print(f"{a} elevado a la {b} igual a = {a ** b}")
+# print(f"{a} elevado a la 4 igual a = {a ** 4}")
+#
+# print(f"La raiz cuadrada de {a} es igual a {a ** .5}")
